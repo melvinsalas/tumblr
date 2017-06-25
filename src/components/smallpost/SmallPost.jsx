270c05cbaf1b198dd5790b6ad18ac40942b2950a
Melvin Salas <msalas@coinca.tv>
2017-06-25 16:01:54 -0600

    fix small post

--- src/components/smallpost/SmallPost.jsx
+++ src/components/smallpost/SmallPost.jsx
@@ -4,6 +4,8 @@ import { bindActionCreators } from 'redux';
 import * as testActions from '../../actions/testActions';
 import '../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js';
 import '../../../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+import LikeImage from '../../assets/images/post/like.svg';
+import ReblogImage from '../../assets/images/post/reblog.svg';
 
 require('./smallpost.scss');
 
@@ -21,8 +23,8 @@ class SmallPost extends React.Component {
                 <div className="footer">
                     <span className="count">234,450 notes</span>
                     <div className="actions">
-                        <img className="btn-footer" src="src/assets/images/post/like.svg" alt="Like" />
-                        <img className="btn-footer" src="src/assets/images/post/reblog.svg" alt="Reblog" />
+                        <img className="btn-footer" src={LikeImage} alt="Like" />
+                        <img className="btn-footer" src={ReblogImage} alt="Reblog" />
                     </div>
                 </div>
             </div>
